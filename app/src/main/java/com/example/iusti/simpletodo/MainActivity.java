c6da0c5db5b47eaabfabc6e85329201c75bba9af
Boiculese Claudiu <Boiculese Claudiu>
2016-10-08 11:47:28 +0300

    initial commit

---
+++ app/src/main/java/com/example/iusti/simpletodo/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.iusti.simpletodo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String[] items = {"Bread" ,"Fruits", "Ice-Cream","Cheese","Potatoes",
+            "Milk", "Flowers" ,"Vegetables", "Meat", "Rice", "Pizza" , "Water" , "Chips"} ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView listView = (ListView) findViewById(R.id.list);
+
+    }
+
+
+}
